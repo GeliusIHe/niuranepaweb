a613d07c4eedded3e50ac32bed3d099909f97785
Nikita Kashirsky <rffaatg@gmail.com>
2024-10-05 19:22:07 +0300

    feat: Add Yandex Metrica tracking script
    - Integrated Yandex Metrica script for enhanced tracking analytics
    - Implemented script integration using dangerouslySetInnerHTML
    - Ensured noscript tag for non-JS environments with hidden image tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,17 +19,45 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+      <html lang="en">
+      <head>
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(98556729, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true
+              });
+            `,
+            }}
+        />
+        <noscript>
+          <div>
+            <img
+                src="https://mc.yandex.ru/watch/98556729"
+                style={{ position: 'absolute', left: '-9999px' }}
+                alt=""
+            />
+          </div>
+        </noscript>
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      {children}
       </body>
-    </html>
+      </html>
   );
 }
